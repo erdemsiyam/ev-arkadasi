e7f0cd30411243333c7bbe2d791bea37dffd186c
Erdem Siyam <erdemsiyam@gmail.com>
2022-05-12 06:50:12 +0300

    harita entegre

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,19 +1,45 @@
 import 'package:ev_arkadasi/filter/filter_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
-          ),
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
         ),
+        // Center(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: <Widget>[],
+        //   ),
+        // ),
       ),
       floatingActionButton: Row(
         mainAxisSize: MainAxisSize.max,
@@ -21,6 +47,7 @@ class HomePage extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           FloatingActionButton(
+            heroTag: "1",
             onPressed: () {
               Navigator.push(
                 context,
@@ -33,7 +60,11 @@ class HomePage extends StatelessWidget {
             child: const Icon(Icons.sort),
           ),
           FloatingActionButton(
-            onPressed: () {},
+            heroTag: "2",
+            onPressed: () async {
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+            },
             tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
