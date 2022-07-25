878e49dce0ed768a9b1e8604bf07aac9c117d386
Erdem Siyam <erdemsiyam@gmail.com>
2022-07-25 23:16:19 +0300

    Servis Auth kısmı testlerinin başarıyla tamamlanması

--- test/services_test.dart
+++ test/services_test.dart
@@ -1,12 +1,57 @@
 import 'package:ev_arkadasi/model/rent_model.dart';
+import 'package:ev_arkadasi/repository/user_repository.dart';
 import 'package:ev_arkadasi/service/rent/rent_service.dart';
+import 'package:ev_arkadasi/service/user/user_service.dart';
+import 'package:ev_arkadasi/util/model/user_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() async {
   // Auth
-  // TODO: SIGNUP
-  // TODO: TOKEN
-  // TODO: REFRESH_TOKEN
+  test('SIGNUP', () async {
+    User? response = await UserService.instance.signUp(
+      user: User()
+        ..fromMap(
+          {
+            "name": "Erdem",
+            "surname": "Siyam",
+            "country_code": "TR",
+            "phone": "+905581327535",
+            "sms_code": "1234",
+            "sms_code_expiration_datetime": "2022-07-14 01:20:23",
+            "age": 25,
+            "gender": 1,
+            "job": 1,
+            "register_datetime": "2022-07-14 01:20:22",
+            "last_login_datetime": "2022-07-14 01:20:22",
+            "instagram": "erdemsym",
+            "facebook": "erdemsym",
+            "description":
+                "Açıklamadır Açıklamadır Açıklamadır Açıklamadır Açıklamadır Açıklamadır Açıklamadır"
+          },
+        ),
+    );
+    UserRepository.instance.user = response;
+    expect(response, isNot(null));
+  });
+  test('TOKEN', () async {
+    User? user = User();
+    user.phone = "+905511231234";
+    user.smsCode = "1234";
+    User? response = await UserService.instance.login(user: user);
+    UserRepository.instance.user = response;
+    expect(response, isNot(null));
+  });
+  test('REFRESH_TOKEN', () async {
+    // User? user = User();
+    // user.phone = "+905511231234";
+    // user.smsCode = "1234";
+    // user = await UserService.instance.login(user: user);
+    // UserRepository.instance.user = user;
+    User? response = await UserService.instance.refreshToken(
+        refreshToken: UserRepository.instance.user!.refreshToken!);
+    UserRepository.instance.user = response;
+    expect(response, isNot(null));
+  });
 
   // User
   // TODO: GET_USER
