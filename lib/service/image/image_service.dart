5d62efe8efaf3ffc6d6b35dc25c546353d89bdae
Erdem Siyam <erdemsiyam@gmail.com>
2022-07-31 17:29:46 +0300

    image servis oluşturulması ve response modeli

---
+++ lib/service/image/image_service.dart
@@ -0,0 +1,24 @@
+import 'package:ev_arkadasi/service/image/model/image_response_model.dart';
+import 'package:ev_arkadasi/util/base/base_service.dart';
+import 'package:ev_arkadasi/util/constant/service_method.dart';
+
+class ImageService extends BaseService {
+  // Singleton
+  ImageService._singleton();
+  static final ImageService _instance = ImageService._singleton();
+  static ImageService get instance => _instance;
+
+  // Methods
+  Future<ImageResponseModel?> userImageAdd(
+      {required String refreshToken}) async {
+    try {
+      return await request<ImageResponseModel, ImageResponseModel>(
+        responseModel: ImageResponseModel(),
+        serviceMethod: ServiceMethod.USER_IMAGE_ADD,
+        isRefreshToken: true,
+      );
+    } catch (e) {
+      return null;
+    }
+  }
+}
---
+++ lib/service/image/model/image_response_model.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+
+import 'package:ev_arkadasi/util/base/base_model.dart';
+
+class ImageResponseModel extends BaseModel {
+  ImageResponseModel({
+    this.uuid,
+    this.userUuid,
+    this.rentUuid,
+    this.index,
+    this.bigUrl,
+    this.smallUrl,
+    this.createdDate,
+  });
+
+  String? uuid;
+  String? userUuid;
+  String? rentUuid;
+  int? index;
+  String? bigUrl;
+  String? smallUrl;
+  DateTime? createdDate;
+
+  @override
+  String toJson() => json.encode({
+        "uuid": uuid,
+        "user_uuid": userUuid,
+        "rent_uuid": rentUuid,
+        "index": index,
+        "big_url": bigUrl,
+        "small_url": smallUrl,
+        "created_date": createdDate?.toIso8601String() ?? "",
+      });
+
+  factory ImageResponseModel.fromJson(Map<String, dynamic> json) =>
+      ImageResponseModel();
+  @override
+  fromJson(String jsonStr) {
+    fromMap(json.decode(jsonStr));
+  }
+
+  @override
+  fromMap(Map<String, dynamic> map) {
+    uuid = map["uuid"];
+    userUuid = map["user_uuid"];
+    rentUuid = map["rent_uuid"];
+    index = map["index"];
+    bigUrl = map["big_url"];
+    smallUrl = map["small_url"];
+    createdDate = DateTime.parse(map["created_date"]);
+  }
+}
